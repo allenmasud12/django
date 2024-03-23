de8a404439094603d6e0638af64eff78353a5600
Allen Masud <100032855+allenmasud12@users.noreply.github.com>
2024-03-23 13:41:47 +0600

    math operators

--- Project1/myapp/views.py
+++ Project1/myapp/views.py
@@ -6,3 +6,14 @@ def home(request):
     text = {'name': 'Allen Masud', 'work': 'He is a android app developer'}
     return render(request, 'index.html', text)
     
+def add(request):
+    value1 = int(request.POST['num1'])
+    value2 = int(request.POST['num2'])
+    values = value1 + value2
+    values1 = value1 - value2
+    values2 = value1 * value2
+    values3 = value1 / value2
+    values4 = value1 % value2
+    values5 = value1 ** value2
+    values6 = value1 // value2
+    return render(request, 'result.html', {'result': values, 'result1':values1, 'result2':values2, 'result3':values3, 'result4':values4, 'result5':values5, 'result6':values6})
