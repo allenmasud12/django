2c1294b3dadf6c80b93531fd38df70179b0aae56
Allen Masud <100032855+allenmasud12@users.noreply.github.com>
2024-03-23 12:33:15 +0600

    json data connected to html file

--- Project1/myapp/views.py
+++ Project1/myapp/views.py
@@ -3,4 +3,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return render(request, 'index.html')
+    text = {'name': 'Allen Masud', 'work': 'He is a android app developer'}
+    return render(request, 'index.html', text)
+    
